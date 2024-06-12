a12214480b8560fa9e3ca218631859f5cd6aadae
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-06-13 00:22:09 +0400

    feat: create custom money cast which casts product's price to money class

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,10 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
 
     public function user(): BelongsTo
     {
